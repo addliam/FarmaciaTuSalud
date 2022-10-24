b849907dd75c552ce642ad2aeb736b935cc2e857
addliam <appplecdos504@gmail.com>
2022-10-24 12:28:31 -0500

    modelo Usuario

---
+++ src/main/java/backend/model/Usuario.java
@@ -0,0 +1,58 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package backend.model;
+
+/**
+ *
+ * @author codeli4m
+ */
+public class Usuario {
+    
+    private int id;
+    private String nombre;
+    private String nickname;    
+    private String password;
+    private String rol;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRol() {
+        return rol;
+    }
+
+    public void setRol(String rol) {
+        this.rol = rol;
+    }    
+}
